863c79e76d082205a67bec5cd9f0ae726d6afdeb
Radhika10128 <radhikasharma10128@gmail.com>
2020-06-15 18:14:31 +0530

    added photowall project

---
+++ src/components/images.js
@@ -0,0 +1,5 @@
+import first from '.././first.jpg';
+import second from '.././second.png';
+import third from '.././third.jpg'
+let images=[first,second,third];
+export default images;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/main.js'
+import './stylesheet.css'
+ReactDOM.render(
+  <Main/>,
+  document.getElementById('root')
+);
